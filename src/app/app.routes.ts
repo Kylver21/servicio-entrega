6ce5b3dd88650a57eed82afd09d6140dd3746565
Angelo270204 <angelow.piscis.27@gmail.com>
2025-06-11 01:17:57 -0500

    Cambios en el HomeComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from "@angular/router";
 import { Home } from "./components/home/home";
+import { CrearPaqueteComponent } from "./components/crear-paquete/crear-paquete";
 
 export const routes: Routes = [
   {
     path: "",
     component: Home,
+  },
+  {
+    path: "crear-paquete",
+    component: CrearPaqueteComponent,
   },
 ];
