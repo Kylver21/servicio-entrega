a19516bb32c64e40be6c1e7d0c09d72918089ac4
Kylver21 <kilverpaucar1@gmail.com>
2025-06-11 00:35:48 -0500

    crea-paquete

--- src/app/components/crear-paquete/crear-paquete.ts
+++ src/app/components/crear-paquete/crear-paquete.ts
@@ -1,22 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProductoService } from '../../services/producto';
+import { Observable } from 'rxjs';
+import { Producto } from '../../models/producto.model';
 
 @Component({
   selector: 'app-crear-paquete',
   standalone: true,
-  imports: [
-  CommonModule,
-
-  ],
+  imports: [CommonModule],
   templateUrl: './crear-paquete.html',
   styleUrls: ['./crear-paquete.scss'],
 })
 export class CrearPaqueteComponent {
+  productos$: Observable<Producto[]> = inject(ProductoService).obtenerProductos();
 
   showPackageTypeSelection: boolean = false;
 
   constructor() {
     console.log('Componente CrearPaqueteComponent ha sido creado.');
   }
-
 }
